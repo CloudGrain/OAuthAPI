1b6e2f5fd734d57840bec5d23147f1b39443137d
ashokkum <ashok.kumar@basware.com>
2022-08-08 11:19:24 +0530

    Initial commit

---
+++ Models/LoginModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace OAuthV2API.Models
+{
+    public class LoginModel
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [JsonIgnore]
+        public long Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        [JsonIgnore]
+        public string RefreshToken { get; set; }
+        [JsonIgnore]
+        public DateTime RefreshTokenExpiryTime { get; set; }
+    }
+}
---
+++ Models/UserContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OAuthV2API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OAuthV2API.Models
+{
+    public class UserContext: DbContext
+    {
+        public UserContext(DbContextOptions dbContextOptions)
+       : base(dbContextOptions)
+        {
+        }
+        public DbSet<LoginModel> LoginModels { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<LoginModel>().HasData(new LoginModel
+            {
+                Id = 1,
+                UserName = "[UserName]",
+                Password = "[Password]"
+            });
+        }
+    }
+}
